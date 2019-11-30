83a03e5134243d8528e8a36da52f48bfdf051c74
zhiganovdmitry <57540867+zhiganovdmitry@users.noreply.github.com>
2019-11-30 23:32:46 +0300

    Update dz3.cpp

--- dz3.cpp
+++ dz3.cpp
@@ -1,36 +1,24 @@
 #include <iostream>
-#include <math.h>
+#include<vector>
 using namespace std;
-int simple(int a)
-{
-	int count=0; 
-	if(a<2)
-	{
-		return 0;	
-	}
-	for(int i=2;i<sqrt(a)+1;++i)
-	{
-		if(a%i==0)
-		{
-			++count;
-		}
-	}
-	if((count==0)||(a==2))
-	{
-		return 1;
-	} else
-	{
-		return 0;
-	}
-}
-
 int main() {
 	int n;
-	int count=1;
 	cin>>n;
+vector<bool> prostye (n + 1, true);
+prostye[0] = prostye[1] = false;
+for (int i = 2; i * i <= n; ++i)
+{
+    if (prostye[i])
+    {
+        for (int j = i * i; j <= n; j += i)
+	{
+            prostye[j] = false;
+	}
+    }
+}
 	for(int i=2;i<n+1;++i)
 	{
-		if(!simple(i))
+		if(!prostye[i])
 		{
 			cout<<i<<"\n";
 		}
