a1d9b62dfa1063b75d6af4d7160cd774448f2f1c
zhiganovdmitry <57540867+zhiganovdmitry@users.noreply.github.com>
2019-11-30 23:47:37 +0300

    Update dz8.cpp

--- dz8.cpp
+++ dz8.cpp
@@ -1,36 +1,25 @@
 #include <iostream>
-#include <math.h>
+#include <vector>
 using namespace std;
-int simple(int a)
-{
-	int count=0; 
-	if(a<2)
-	{
-		return 0;	
-	}
-	for(int i=2;i<sqrt(a)+1;++i)
-	{
-		if(a%i==0)
-		{
-			++count;
-		}
-	}
-	if((count==0)||(a==2))
-	{
-		return 1;
-	} else
-	{
-		return 0;
-	}
-}
-
 int main() {
 	int n;
-	int count=1;
 	cin>>n;
+vector<bool> prostye (n + 1, true);
+prostye[0] = prostye[1] = false;
+for (int i = 2; i * i <= n; ++i)
+{
+    if (prostye[i])
+    {
+        for (int j = i * i; j <= n; j += i)
+	{
+            prostye[j] = false;
+	}
+    }
+}
+	int count=1;
 	for(int i=2;i<n+1;++i)
 	{
-		if(simple(i))
+		if(prostye[i])
 		{
 			count*=i;
 		}
